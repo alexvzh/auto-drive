acaf199ecaed18dd064e92118792e27cf99eee00
alexvhz <alexvictor1305@gmail.com>
2024-11-21 02:55:35 +0100

    Add MenuScene implementation

--- src/main/java/scene/scenes/MenuScene.java
+++ src/main/java/scene/scenes/MenuScene.java
@@ -1,22 +1,36 @@
 package scene.scenes;
 
+import object.MenuBall;
 import scene.Scene;
 import scene.SceneManager;
 
 import java.awt.*;
+import java.util.Random;
 
 public class MenuScene extends Scene {
+
+    Random r = new Random();
+
     public MenuScene(String id, SceneManager sceneManager) {
-        super(100, 100, id, sceneManager);
+        super(0, 0, id, sceneManager);
+        this.setLayout(null);
+        spawnBalls();
+        addButton((getPreferredSize().width/2)-125,(getPreferredSize().height/3)-50, 250, 100, "Test", e -> System.out.println(getWidth()/2));
     }
 
     @Override
     public void update() {
+        objectHandler.update();
     }
 
     @Override
     public void draw(Graphics2D g2d) {
-        g2d.setColor(Color.WHITE);
-        g2d.drawString("ag", 100, 100);
+        objectHandler.draw(g2d);
+    }
+
+    private void spawnBalls() {
+        for (int i = 0; i < 150; i ++) {
+            new MenuBall(r.nextInt(1100), r.nextInt(800), objectHandler);
+        }
     }
 }
