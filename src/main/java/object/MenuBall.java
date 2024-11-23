223fbc97964f77d3bdf82e8ac2d22c37b3035660
alexvhz <alexvictor1305@gmail.com>
2024-11-23 21:16:51 +0100

    Change range of default x and y velocities to include all orientations and modify sign change conditions

--- src/main/java/object/MenuBall.java
+++ src/main/java/object/MenuBall.java
@@ -8,6 +8,7 @@ import java.util.Random;
 
 public class MenuBall extends Object {
 
+    private final Scene scene;
     private final Color color;
     private final int size;
     private double velX;
@@ -15,10 +16,11 @@ public class MenuBall extends Object {
 
     public MenuBall(double x, double y, Scene scene) {
         super(x, y, scene);
+        this.scene = scene;
         Random r = new Random();
         this.size = r.nextInt(10) + 8;
-        velX = (double) (r.nextInt(60) + 5)/ 100;
-        velY = (double) (r.nextInt(60) + 5)/ 100;
+        velX = (double) (r.nextInt(400) - 200) / 100;
+        velY = (double) (r.nextInt(400) - 200) / 100;
         scene.getObjectHandler().addObject(this);
         this.color = Arrays.asList(Color.DARK_GRAY, Color.LIGHT_GRAY, Color.WHITE).get(r.nextInt(3));
     }
@@ -29,8 +31,8 @@ public class MenuBall extends Object {
         x+=velX;
         y+=velY;
 
-        if (x < 0 - (double) size / 2 || x > 1128 - (double) size / 2) velX *= -1;
-        if (y < 0 - (double) size / 2 || y > 848 - (double) size / 2) velY *= -1;
+        if (x < 0 || x > scene.getWidth() - (double) size) velX *= -1;
+        if (y < 0 || y > scene.getHeight() - (double) size) velY *= -1;
 
     }
 
