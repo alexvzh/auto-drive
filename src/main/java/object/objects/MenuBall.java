1d5a20c94baf42add449c1967a71da741f476b2c
alexvhz <alexvictor1305@gmail.com>
2024-11-30 17:11:33 +0100

    Add and implement createCircleImage() method in MenuBall class to improve circle rendering efficiency

--- src/main/java/object/objects/MenuBall.java
+++ src/main/java/object/objects/MenuBall.java
@@ -4,6 +4,7 @@ import object.Object;
 import scene.Scene;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -14,23 +15,25 @@ public class MenuBall extends Object {
     private final int size;
     private double velX;
     private double velY;
+    private final BufferedImage circleImage;
 
     public MenuBall(double x, double y, Scene scene) {
         super(x, y, scene);
         this.scene = scene;
         Random r = new Random();
         this.size = r.nextInt(10) + 8;
-        velX = (double) (r.nextInt(400) - 200) / 100;
-        velY = (double) (r.nextInt(400) - 200) / 100;
+        this.velX = (double) (r.nextInt(400) - 200) / 100;
+        this.velY = (double) (r.nextInt(400) - 200) / 100;
         scene.getObjectHandler().addObject(this);
         this.color = Arrays.asList(Color.DARK_GRAY, Color.LIGHT_GRAY, Color.WHITE).get(r.nextInt(3));
+        this.circleImage =  createCircleImage(size, color);
     }
 
     @Override
     public void update() {
 
-        x+=velX;
-        y+=velY;
+        x += velX;
+        y += velY;
 
         if (x < 0 || x > scene.getWidth() - (double) size) velX *= -1;
         if (y < 0 || y > scene.getHeight() - (double) size) velY *= -1;
@@ -40,6 +43,23 @@ public class MenuBall extends Object {
     @Override
     public void draw(Graphics2D g2d) {
         g2d.setColor(color);
-        g2d.fillOval((int)x, (int)y, size, size);
+        g2d.drawImage(circleImage, (int) x, (int) y, null);
+
     }
+
+
+    public static BufferedImage createCircleImage(int diameter, Color color) {
+        BufferedImage circleImage = new BufferedImage(diameter, diameter, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = circleImage.createGraphics();
+        // Enable antialiasing for smooth edges
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2d.setColor(color);
+        g2d.fillOval(0, 0, diameter, diameter);
+
+        g2d.dispose();
+        return circleImage;
+
+    }
+
 }
