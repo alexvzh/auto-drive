a2fa9bd265c68376324a84e967b6313177381be5
alexvhz <alexvictor1305@gmail.com>
2024-11-30 23:53:27 +0100

    Add super constructor and methods to Expander class

--- src/main/java/object/guiobjects/Expander.java
+++ src/main/java/object/guiobjects/Expander.java
@@ -1,4 +1,25 @@
 package object.guiobjects;
 
-public class Expander {
+import object.Object;
+import object.behaviour.Drawable;
+import object.behaviour.Updatable;
+import scene.Scene;
+
+import java.awt.*;
+
+public class Expander extends Object implements Updatable, Drawable {
+
+    public Expander(double x, double y, Scene scene) {
+        super(x, y, scene);
+    }
+
+    @Override
+    public void draw(Graphics2D g2d) {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
 }
