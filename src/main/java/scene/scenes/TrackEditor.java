9295d4d6f56f99b47a1c35bc3460a17ee87fb868
alexvhz <alexvictor1305@gmail.com>
2024-11-30 22:51:27 +0100

    Make TrackEditor extend Scene and implement super methods and constructor

--- src/main/java/scene/scenes/TrackEditor.java
+++ src/main/java/scene/scenes/TrackEditor.java
@@ -1,4 +1,28 @@
 package scene.scenes;
 
-public class TrackEditor {
+import scene.Scene;
+import scene.SceneFrequency;
+import scene.SceneManager;
+
+import java.awt.*;
+
+public class TrackEditor extends Scene {
+    public TrackEditor(int width, int height, int initialObjectCapacity, String id, SceneFrequency sceneFrequency, SceneManager sceneManager) {
+        super(width, height, initialObjectCapacity, id, sceneFrequency, sceneManager);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void draw(Graphics2D g2d) {
+
+    }
+
+    @Override
+    public void init() {
+
+    }
 }
