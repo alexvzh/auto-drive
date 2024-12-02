7f414dfabd74b8c2d15d1603a96e2237be8a028f
alexvhz <alexvictor1305@gmail.com>
2024-12-03 00:46:34 +0100

    Implement abstract methods in ExpanderElement class

--- src/main/java/object/guiobjects/ExpanderElement.java
+++ src/main/java/object/guiobjects/ExpanderElement.java
@@ -1,4 +1,42 @@
 package object.guiobjects;
 
-public class ExpanderElement {
+import object.Object;
+import object.behaviour.Clickable;
+import object.behaviour.Drawable;
+import object.behaviour.Hoverable;
+import scene.Scene;
+
+import java.awt.*;
+
+public class ExpanderElement extends Object implements Drawable, Clickable, Hoverable {
+
+    public ExpanderElement(double x, double y, Scene scene) {
+        super(x, y, scene);
+    }
+
+    @Override
+    public void draw(Graphics2D g2d) {
+
+    }
+
+    @Override
+    public void onClick() {
+
+    }
+
+    @Override
+    public void onHover() {
+
+    }
+
+    @Override
+    public void onUnhover() {
+
+    }
+
+    @Override
+    public Rectangle getBounds() {
+        return null;
+    }
+
 }
