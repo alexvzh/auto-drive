fcd02be6748ef2bbac9c97f0ad0fc3ca6aee4691
alexvhz <alexvictor1305@gmail.com>
2024-12-02 18:15:26 +0100

    Refactor createCollapsedIcon() and createExpandedIcon() into a single createIcon() method with ExpansionState parameter

--- src/main/java/object/guiobjects/Expander.java
+++ src/main/java/object/guiobjects/Expander.java
@@ -15,8 +15,8 @@ public class Expander extends Object implements Updatable, Drawable {
         EXPANDED
     }
 
-    private static final BufferedImage collapsedIcon = createCollapsedIcon();
-    private static final BufferedImage expandedIcon = createExpandedIcon();
+    private static final BufferedImage collapsedIcon = createIcon(ExpansionState.COLLAPSED);
+    private static final BufferedImage expandedIcon = createIcon(ExpansionState.EXPANDED);
 
     public Expander(double x, double y, Scene scene) {
         super(x, y, scene);
@@ -32,7 +32,7 @@ public class Expander extends Object implements Updatable, Drawable {
 
     }
 
-    private static BufferedImage createCollapsedIcon() {
+    private static BufferedImage createIcon(ExpansionState state) {
         int size = 50;
 
         BufferedImage image = new BufferedImage(size, size/2, BufferedImage.TYPE_INT_ARGB);
@@ -44,28 +44,13 @@ public class Expander extends Object implements Updatable, Drawable {
 
         g2d.setColor(Color.LIGHT_GRAY);
         g2d.setStroke(new BasicStroke(3));
-        g2d.drawLine(size/7, size/2-size/7, size/2, size/7);
-        g2d.drawLine(size/2, size/7, size - size/7, size/2 - size/7);
-
-        g2d.dispose();
-
-        return image;
-    }
-
-    private static BufferedImage createExpandedIcon() {
-        int size = 50;
-
-        BufferedImage image = new BufferedImage(size, size/2, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2d = image.createGraphics();
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-        g2d.setColor(new Color(0, 0, 0, 128));
-        g2d.fillRect(0, 0, size, size);
-
-        g2d.setColor(Color.LIGHT_GRAY);
-        g2d.setStroke(new BasicStroke(3));
-        g2d.drawLine(size/7, size/7, size/2, size/2-size/7);
-        g2d.drawLine(size/2, size/2-size/7, size - size/7, size/7);
+        if (state == ExpansionState.EXPANDED) {
+            g2d.drawLine(size/7, size/7, size/2, size/2-size/7);
+            g2d.drawLine(size/2, size/2-size/7, size - size/7, size/7);
+        } else {
+            g2d.drawLine(size/7, size/2-size/7, size/2, size/7);
+            g2d.drawLine(size/2, size/7, size - size/7, size/2 - size/7);
+        }
 
         g2d.dispose();
 
