6cdfc4bd5b4e74b08c430e9b5aa8337355188e66
alexvhz <alexvictor1305@gmail.com>
2024-12-02 23:31:25 +0100

    Add functionality to update() and draw() methods in TrackEditor and add the Expander to the Scene

--- src/main/java/scene/scenes/TrackEditor.java
+++ src/main/java/scene/scenes/TrackEditor.java
@@ -1,5 +1,6 @@
 package scene.scenes;
 
+import object.guiobjects.Expander;
 import scene.Scene;
 import scene.SceneFrequency;
 import scene.SceneManager;
@@ -9,16 +10,18 @@ import java.awt.*;
 public class TrackEditor extends Scene {
     public TrackEditor(String id, int initialObjectCapacity, SceneFrequency sceneFrequency, SceneManager sceneManager) {
         super(id, initialObjectCapacity, sceneFrequency, sceneManager);
+        setBackroundColor(Color.WHITE);
+        new Expander((double) getPreferredSize().width/2-25, getPreferredSize().height-30, this);
     }
 
     @Override
     public void update() {
-
+        getObjectHandler().update();
     }
 
     @Override
     public void draw(Graphics2D g2d) {
-
+        getObjectHandler().draw(g2d);
     }
 
     @Override
