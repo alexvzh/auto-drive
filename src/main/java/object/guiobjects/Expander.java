1d935e74c79b3c9251303e01f3b5a45a9247321c
alexvhz <alexvictor1305@gmail.com>
2024-12-02 18:11:33 +0100

    Declare static final expandedIcon field and initialize it with newly created createExpandedIcon() method

--- src/main/java/object/guiobjects/Expander.java
+++ src/main/java/object/guiobjects/Expander.java
@@ -10,7 +10,8 @@ import java.awt.image.BufferedImage;
 
 public class Expander extends Object implements Updatable, Drawable {
 
-    private static final BufferedImage collapsedIcon;
+    private static final BufferedImage collapsedIcon = createCollapsedIcon();
+    private static final BufferedImage expandedIcon = createExpandedIcon();
 
     public Expander(double x, double y, Scene scene) {
         super(x, y, scene);
@@ -40,6 +41,26 @@ public class Expander extends Object implements Updatable, Drawable {
         g2d.setStroke(new BasicStroke(3));
         g2d.drawLine(size/7, size/2-size/7, size/2, size/7);
         g2d.drawLine(size/2, size/7, size - size/7, size/2 - size/7);
+
+        g2d.dispose();
+
+        return image;
+    }
+
+    private static BufferedImage createExpandedIcon() {
+        int size = 50;
+
+        BufferedImage image = new BufferedImage(size, size/2, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = image.createGraphics();
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2d.setColor(new Color(0, 0, 0, 128));
+        g2d.fillRect(0, 0, size, size);
+
+        g2d.setColor(Color.LIGHT_GRAY);
+        g2d.setStroke(new BasicStroke(3));
+        g2d.drawLine(size/7, size/7, size/2, size/2-size/7);
+        g2d.drawLine(size/2, size/2-size/7, size - size/7, size/7);
 
         g2d.dispose();
 
